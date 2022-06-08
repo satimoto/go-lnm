eeaa083cf1b58f4837b5c3e1cf8fe1ce7b50e269
Ross Savage <ross.savage@agrista.com>
2022-06-08 16:51:56 +0200

    Parse getInfo uris to get LSP address

---
+++ internal/util/lightning_addr.go
@@ -0,0 +1,35 @@
+package util
+
+import (
+	"strings"
+)
+
+type LightningAddr struct {
+	Pubkey string
+	Host   string
+}
+
+func NewLightingAddr(lightningAddr string) *LightningAddr {
+	splitLightningAddr := strings.Split(lightningAddr, "@")
+
+	if len(splitLightningAddr) == 2 {
+		return &LightningAddr{
+			Pubkey: splitLightningAddr[0],
+			Host:   splitLightningAddr[1],
+		}
+	}
+
+	return &LightningAddr{
+		Pubkey: lightningAddr,
+	}
+}
+
+func (l *LightningAddr) Hostname () string {
+	hostSplit := strings.Split(l.Host, ":")
+
+	if len(hostSplit) == 2 {
+		return hostSplit[0]
+	}
+
+	return l.Host
+}
