9c0dc0b46f2d5a1988f4ee5cb7c61870c5826847
Ross Savage <ross.savage@agrista.com>
2022-06-08 23:06:09 +0200

    Add terraform configuration

---
+++ terraform/environments/mainnet/main.tf
@@ -0,0 +1,111 @@
+provider "aws" {
+  region                = var.region
+  forbidden_account_ids = var.forbidden_account_ids
+  profile               = "satimoto-mainnet"
+}
+
+provider "aws" {
+  alias                 = "us_east_1"
+  region                = "us-east-1"
+  forbidden_account_ids = var.forbidden_account_ids
+  profile               = "satimoto-mainnet"
+}
+
+provider "aws" {
+  alias                 = "zone_owner"
+  region                = var.region
+  forbidden_account_ids = var.forbidden_account_ids
+  profile               = "satimoto-common"
+}
+
+# -----------------------------------------------------------------------------
+# Backends
+# -----------------------------------------------------------------------------
+
+data "terraform_remote_state" "infrastructure" {
+  backend = "s3"
+
+  config = {
+    bucket  = "satimoto-terraform-mainnet"
+    key     = "infrastructure.tfstate"
+    region  = "eu-central-1"
+    profile = "satimoto-mainnet"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket  = "satimoto-terraform-mainnet"
+    key     = "lsp.tfstate"
+    region  = "eu-central-1"
+    profile = "satimoto-mainnet"
+  }
+}
+
+# -----------------------------------------------------------------------------
+# Modules
+# -----------------------------------------------------------------------------
+
+module "subdomain_zone" {
+  providers = {
+    aws.zone_owner = aws.zone_owner
+  }
+  count              = var.lsp_count
+  source             = "git::https://github.com/satimoto/terraform-infrastructure.git//modules/subdomain-zone?ref=develop"
+  availability_zones = var.availability_zones
+  region             = var.region
+
+  domain_name     = data.terraform_remote_state.infrastructure.outputs.route53_zone_name
+  subdomain_name  = "lsp${count.index + 1}"
+  route53_zone_id = data.terraform_remote_state.infrastructure.outputs.route53_zone_id
+}
+
+resource "aws_security_group_rule" "nat_any_btc_p2p_egress_rule" {
+  type              = "egress"
+  from_port         = 8333
+  to_port           = 8333
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = data.terraform_remote_state.infrastructure.outputs.nat_security_group_id
+  description       = "BTC-P2P to Any from NAT"
+}
+
+resource "aws_security_group_rule" "nat_any_lnd_p2p_egress_rule" {
+  type              = "egress"
+  from_port         = var.lnd_p2p_port
+  to_port           = var.lnd_p2p_port
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = data.terraform_remote_state.infrastructure.outputs.nat_security_group_id
+  description       = "LND-P2P to Any from NAT"
+}
+
+module "lsp" {
+  count              = var.lsp_count
+  source             = "../../modules/lsp"
+  availability_zones = var.availability_zones
+  region             = var.region
+
+  vpc_id                = data.terraform_remote_state.infrastructure.outputs.vpc_id
+  private_subnet_ids    = data.terraform_remote_state.infrastructure.outputs.private_subnet_ids
+  public_subnet_cidrs   = data.terraform_remote_state.infrastructure.outputs.public_subnet_cidrs
+  public_subnet_ids     = data.terraform_remote_state.infrastructure.outputs.public_subnet_ids
+  security_group_name   = "lsp${count.index + 1}-instance"
+  ecs_security_group_id = data.terraform_remote_state.infrastructure.outputs.ecs_security_group_id
+  nat_security_group_id = data.terraform_remote_state.infrastructure.outputs.nat_security_group_id
+  rds_security_group_id = data.terraform_remote_state.infrastructure.outputs.rds_security_group_id
+  instance_number       = count.index
+  instance_name         = "LSP${count.index + 1}"
+  ec2_instance_type     = var.ec2_instance_type
+  ec2_key_name          = "lsp${count.index + 1}"
+  ebs_iops              = 3000
+  ebs_size              = 30
+  ebs_type              = "gp3"
+  ebs_throughput        = 125
+  route53_zone_id       = module.subdomain_zone[count.index].route53_zone_id
+  route53_domain_name   = "lsp${count.index + 1}.${data.terraform_remote_state.infrastructure.outputs.route53_zone_name}"
+  rest_port             = var.rest_port
+  rpc_port              = var.rpc_port
+  btc_p2p_port          = var.btc_p2p_port
+  lnd_p2p_port          = var.lnd_p2p_port
+}
---
+++ terraform/environments/mainnet/variables.tf
@@ -0,0 +1,51 @@
+variable "region" {
+  description = "The AWS region"
+  default     = "eu-central-1"
+}
+
+variable "availability_zones" {
+  description = "A list of Availability Zones where subnets and DB instances can be created"
+}
+
+variable "deployment_stage" {
+  description = "The deployment stage"
+  default     = "mainnet"
+}
+
+variable "forbidden_account_ids" {
+  description = "The forbidden account IDs"
+  type        = list(string)
+  default     = []
+}
+
+# -----------------------------------------------------------------------------
+# Module lsp
+# -----------------------------------------------------------------------------
+
+variable "lsp_count" {
+  description = "Number of LSPs to provision"
+}
+
+variable "ec2_instance_type" {
+  description = "The instance type to use for the EC2 instance"
+}
+
+variable "rest_port" {
+  description = "The port to access metrics and health"
+  default     = 9002
+}
+
+variable "rpc_port" {
+  description = "The port to access RPC server"
+  default     = 50000
+}
+
+variable "btc_p2p_port" {
+  description = "The port bitcoind uses for p2p connections"
+  default     = 8333
+}
+
+variable "lnd_p2p_port" {
+  description = "The port lnd uses for p2p connections"
+  default     = 9735
+}
