db397cb6ef3edd22512bdd36497e03eb957e2198
Ross Savage <ross.savage@agrista.com>
2022-08-14 23:39:44 +0200

    Add channel acceptor monitor. Closes #3

---
+++ internal/monitor/channelacceptor/mocks/mock_channel_acceptor.go
@@ -0,0 +1,15 @@
+package mocks
+
+import (
+	mocks "github.com/satimoto/go-datastore/pkg/db/mocks"
+	channelrequest "github.com/satimoto/go-lsp/internal/channelrequest/mocks"
+	lightningnetwork "github.com/satimoto/go-lsp/internal/lightningnetwork/mocks"
+	"github.com/satimoto/go-lsp/internal/monitor/channelevent"
+)
+
+func NewChannelEventMonitor(repositoryService *mocks.MockRepositoryService, lightningService *lightningnetwork.MockLightningNetworkService) *channelevent.ChannelEventMonitor {
+	return &channelevent.ChannelEventMonitor{
+		LightningService:       lightningService,
+		ChannelRequestResolver: channelrequest.NewResolver(repositoryService),
+	}
+}
