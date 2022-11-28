0f8849d22972476a3e4e9788428edbd6bcc9397b
Ross Savage <ross.savage@agrista.com>
2022-11-28 15:53:50 +0100

    Immediately send rebate invoice request, add timeout the invoice request payment

--- internal/notification/service.go
+++ internal/notification/service.go
@@ -4,12 +4,15 @@ import (
 	"log"
 
 	"github.com/appleboy/go-fcm"
+	"github.com/satimoto/go-datastore/pkg/db"
 	"github.com/satimoto/go-datastore/pkg/util"
+	metrics "github.com/satimoto/go-lsp/internal/metric"
 )
 
 type Notification interface {
 	SendNotification(*fcm.Message) (*fcm.Response, error)
 	SendNotificationWithRetry(message *fcm.Message, retries int) (*fcm.Response, error)
+	SendUserNotification(user db.User, data NotificationDto, notificationType string)
 }
 
 type NotificationService struct {
@@ -35,4 +38,22 @@ func (s *NotificationService) SendNotificationWithRetry(message *fcm.Message, re
 	log.Printf("Sending notification with retry: %v", message.To)
 	log.Printf("Data=%#v", message.Data)
 	return s.client.SendWithRetry(message, retries)
+}
+
+func (s *NotificationService) SendUserNotification(user db.User, data NotificationDto, notificationType string) {
+	message := &fcm.Message{
+		To:               user.DeviceToken.String,
+		ContentAvailable: true,
+		Data:             data,
+	}
+
+	_, err := s.SendNotificationWithRetry(message, 10)
+
+	if err != nil {
+		// TODO: Cancel session?
+		metrics.RecordError("LSP059", "Error sending notification", err)
+		log.Printf("LSP059: Message=%v", message)
+	}
+
+	RecordNotificationSent(notificationType, 1)
 }
--- internal/session/notification.go
+++ internal/session/notification.go
@@ -1,40 +1,18 @@
 package session
 
 import (
-	"log"
-
-	"github.com/appleboy/go-fcm"
 	"github.com/satimoto/go-datastore/pkg/db"
-	metrics "github.com/satimoto/go-lsp/internal/metric"
 	"github.com/satimoto/go-lsp/internal/notification"
 )
 
 func (r *SessionResolver) SendSessionInvoiceNotification(user db.User, session db.Session, sessionInvoice db.SessionInvoice) {
 	dto := notification.CreateSessionInvoiceNotificationDto(session, sessionInvoice)
 
-	r.sendNotification(user, dto, notification.SESSION_INVOICE)
+	r.NotificationService.SendUserNotification(user, dto, notification.SESSION_INVOICE)
 }
 
 func (r *SessionResolver) SendSessionUpdateNotification(user db.User, session db.Session) {
 	dto := notification.CreateSessionUpdateNotificationDto(session)
 
-	r.sendNotification(user, dto, notification.SESSION_UPDATE)
-}
-
-func (r *SessionResolver) sendNotification(user db.User, data notification.NotificationDto, notificationType string) {
-	message := &fcm.Message{
-		To:               user.DeviceToken.String,
-		ContentAvailable: true,
-		Data:             data,
-	}
-
-	_, err := r.NotificationService.SendNotificationWithRetry(message, 10)
-
-	if err != nil {
-		// TODO: Cancel session?
-		metrics.RecordError("LSP059", "Error sending notification", err)
-		log.Printf("LSP059: Message=%v", message)
-	}
-
-	notification.RecordNotificationSent(notificationType, 1)
+	r.NotificationService.SendUserNotification(user, dto, notification.SESSION_UPDATE)
 }
