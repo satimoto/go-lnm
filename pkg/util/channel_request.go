e4cb6db8f802d900c1dba10295436e63c0a512a6
Ross Savage <ross.savage@agrista.com>
2022-11-29 23:12:04 +0100

    Set a minimum funding amount in the LSP

--- pkg/util/channel_request.go
+++ pkg/util/channel_request.go
@@ -1,5 +1,11 @@
 package util
 
 func CalculateLocalFundingAmount(amount int64) int64 {
-	return int64(float64(amount) * 1.25)
+	localFundingAmount := int64(float64(amount) * 1.25)
+
+	if localFundingAmount < 20000 {
+		localFundingAmount = 20000
+	}
+
+	return localFundingAmount
 }
